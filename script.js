368cff99f74f6321a6f6728156a3bdce7cd7e691
unknown <arataichise@gmail.com>
2022-06-25 21:46:52 +0700

    upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Nhập tên của bạn\n Ví dụ: Nguyễn Văn A");
+ var b=prompt("Nhập ngày sinh của bạn\n Ví dụ:01-01-2001");
+  	if(a== "Nguyễn Diệu Thúy" && b=="25-06-2004")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
